3ed8bf3773cd6707d408fe82395797efea563532
Zoé Ranzy <zoe.ranzy@gmail.com>
2023-10-05 20:10:16 +0200

    footer mostly done, still need wave effect, styling homepage

--- src/routes/Footer.js
+++ src/routes/Footer.js
@@ -2,16 +2,20 @@ import logoAmple from "../img/logoAmple.png";
 import IHECS from "../img/IHECS.png";
 import ixelles from "../img/ixelles.png";
 import weartxl from "../img/weartxl.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
     return(
         <footer>
-            <img src={logoAmple} alt="une personne grosse d'apparence féminine assise sur le mot Ample" />
+            <img src={logoAmple} alt="une personne grosse d'apparence féminine assise sur le mot Ample" className="logoAmple" />
             <address>
                 <ul>
-                    <li>insta</li>
-                    <li>fb</li>
+                    <li><a href="https://www.instagram.com/projet.ample/?hl=fr"><FontAwesomeIcon icon={faInstagram} className="icons"/></a></li>
+                    <li><a href="https://www.youtube.com/"><FontAwesomeIcon icon={faYoutube} className="icons"/></a></li>
                 </ul>
+                <span></span>
                 <a href="mailto:ample.projet@gmail.com">ample.projet@gmail.com</a>
             </address>
             <section>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,7 @@ export default function Home(){
         <main>
             <section className="about">
                 <h1>About us</h1>
-                <h3>
+                <p>
                     Ample, c'est un projet qui parle de la charge mentale d'exister dans un corps gros dans l'espace public.
                     En effet, l'espace public, lieu de rencontre, de socialisation et de communication, n'est pas adapté aux corps gros.
                     Par « gros », on entend tous les corps dont l'accès au monde est réduit par leur grosseur.
@@ -13,17 +13,17 @@ export default function Home(){
                     Au travers du projet Ample, on souhaite non seulement donner la parole et de la visibilité aux personnes grosses, mettre en lumière la grossophobie de l'espace public, montrer la beauté des corps gros, mais aussi sensibiliser les personnes non concernées à leurs propres comportements et biais grossophobes.
                     <br/><br/>
                     <div className="button"><Link to="/infos">+ d'infos</Link></div>
-                </h3>
+                </p>
             </section>
             <section className="">
                 <h1>Nos intervenant.es</h1>
                 <ul>
-                    <li><img src="" alt="" /><h3>Rabi</h3></li>
-                    <li><img src="" alt="" /><h3>Melissa</h3></li>
-                    <li><img src="" alt="" /><h3>Loïc</h3></li>
-                    <li><img src="" alt="" /><h3>Cassandra</h3></li>
-                    <li><img src="" alt="" /><h3>Adeline</h3></li>
-                    <li><img src="" alt="" /><h3>Emma</h3></li>
+                    <li><Link to="/models"><img src="" alt="" /><h3>Rabi</h3></Link></li>
+                    <li><Link to=""><img src="" alt="" /><h3>Melissa</h3></Link></li>
+                    <li><Link to=""><img src="" alt="" /><h3>Loïc</h3></Link></li>
+                    <li><Link to=""><img src="" alt="" /><h3>Cassandra</h3></Link></li>
+                    <li><Link to=""><img src="" alt="" /><h3>Adeline</h3></Link></li>
+                    <li><Link to=""><img src="" alt="" /><h3>Emma</h3></Link></li>
                 </ul>
             </section>
         </main>

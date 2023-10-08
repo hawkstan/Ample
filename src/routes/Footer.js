f6c0bfe9fc4384b0610705c1e70da07836196560
Zoé Ranzy <zoe.ranzy@gmail.com>
2023-10-08 21:17:39 +0200

    better responsive design

--- src/routes/Footer.js
+++ src/routes/Footer.js
@@ -9,16 +9,19 @@ import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 export default function Footer({ style }) {
     return(
         <footer style={style}>
-            <img src={logoAmple} alt="une personne grosse d'apparence féminine assise sur le mot Ample" className="logoAmple" />
-            <address>
-                <ul>
-                    <li><a href="https://www.instagram.com/projet.ample/?hl=fr" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="icons"/></a></li>
-                    <li><a href="https://www.youtube.com/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faYoutube} className="icons"/></a></li>
-                </ul>
-                <span></span>
-                <a href="mailto:ample.projet@gmail.com">ample.projet@gmail.com</a>
-            </address>
-            <section>
+            <section className="contact">
+                <img src={logoAmple} alt="une personne grosse d'apparence féminine assise sur le mot Ample" className="logoAmple" />
+                <address>
+                    <ul>
+                        <li><a href="https://www.instagram.com/projet.ample/?hl=fr" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="icons"/></a></li>
+                        <li><a href="https://www.youtube.com/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faYoutube} className="icons"/></a></li>
+                    </ul>
+                    <span className="mobBar"></span>
+                    <a href="mailto:ample.projet@gmail.com">ample.projet@gmail.com</a>
+                </address>
+                <span className="webBar"></span>
+            </section>
+            <section className="logos">
                 <ul>
                     <li><img src={IHECS} alt="logo de l'IHECS" /></li>
                     <li><img src={weartxl} alt="logo de WeArtXL" /></li>

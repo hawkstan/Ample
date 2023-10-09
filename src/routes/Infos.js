d0d8952d7292b5080eeff8caa7ea8278ef4a4746
Zoé Ranzy <zoe.ranzy@gmail.com>
2023-10-09 03:32:58 +0200

    small updates

--- src/routes/Infos.js
+++ src/routes/Infos.js
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Infos(){
     return (
         <main className="infos">
-            <h3>Un peu d'informations...</h3>
-            <p>texte</p>
+            <section>
+                <h3>Un peu d'informations...</h3>
+                <p>Toutes ces informations proviennent de nombreuses recherches qui nous ont permis de produire nos actuels posts informatifs disponible sur notre
+                    Instagram <a href="https://www.instagram.com/projet.ample/?hl=fr" target="_blank" rel="noreferrer">@projet.ample</a>
+                </p>
+            </section>
+            <section>
+                <p className="hash">#01</p>
+                <p>La grossophobie</p>
+            </section>
+            <section>
+                <p className="hash">#02</p>
+                <p>La charge mentale</p>
+            </section>
         </main>
     );
 };

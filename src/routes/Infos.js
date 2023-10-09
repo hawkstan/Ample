29df6e95304cc2e65e3ba1736040203e0445d89f
Zoé Ranzy <zoe.ranzy@gmail.com>
2023-10-09 18:23:11 +0200

    infos page ok, witing for content

--- src/routes/Infos.js
+++ src/routes/Infos.js
@@ -1,19 +1,30 @@
+import rainbow from "../img/rainbow.svg"; //see if possible to have png instead
+import star from "../img/star2.svg";
+
 export default function Infos(){
     return (
         <main className="infos">
-            <section>
-                <h3>Un peu d'informations...</h3>
+            <section className="title">
+                <img src={rainbow} alt="une vague colorée en 4 bandes correspondant à la palette couleur du site" className="rainbow"></img>
+                <h1>Un peu d'information</h1>
                 <p>Toutes ces informations proviennent de nombreuses recherches qui nous ont permis de produire nos actuels posts informatifs disponible sur notre
                     Instagram <a href="https://www.instagram.com/projet.ample/?hl=fr" target="_blank" rel="noreferrer">@projet.ample</a>
                 </p>
+                <img src={star} alt="une étoile" className="star"></img>
             </section>
-            <section>
+            <section className="instaPost1">
                 <p className="hash">#01</p>
-                <p>La grossophobie</p>
+                <h4>La grossophobie</h4>
+                <article>
+                    <p>lien vers post insta ?</p>
+                </article>
             </section>
-            <section>
+            <section className="instaPost2">
                 <p className="hash">#02</p>
-                <p>La charge mentale</p>
+                <h4>La charge mentale</h4>
+                <article>
+                    <p>lien vers post insta ?</p>
+                </article>
             </section>
         </main>
     );
